b6ed279ce16a4e96a97bb02593ff21a2638139f7
Michael Watts <wattsjmichael@gmail.com>
2022-03-14 07:45:21 -0700

    Adds Random Battles and Collisions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
+    public LayerMask grassLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -23,12 +24,11 @@ public class PlayerController : MonoBehaviour
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
-            
 
             // remove diagonal movement
-            if (input.x != 0) input.y = 0;
-            
-            
+            if (input.x != 0)
+                input.y = 0;
+
             if (input != Vector2.zero)
             {
                 animator.SetFloat("moveX", input.x);
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
+                
+                if (IsWalkable(new Vector3(targetPos.x,targetPos.y-0.5f)))
                 StartCoroutine(Move(targetPos));
             }
         }
@@ -49,10 +51,36 @@ public class PlayerController : MonoBehaviour
         isMoving = true;
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                targetPos,
+                moveSpeed * Time.deltaTime
+            );
             yield return null;
         }
         transform.position = targetPos;
         isMoving = false;
+        CheckForEncounters();
     }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null) 
+        {
+           return false; 
+        }
+        return true;
+    } 
+
+    private void CheckForEncounters()
+    {
+         if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null)
+         {
+            if (Random.Range(1, 101) <= 12)
+            {   
+                Debug.Log("Triggered A Random Battle");
+            }
+         }
+    }
+
 }
