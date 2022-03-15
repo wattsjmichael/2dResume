589ce2eb7a7a1055306c914784d434ea9fc59f23
Michael Watts <wattsjmichael@gmail.com>
2022-03-15 09:01:07 -0700

    Creates Pokemon Class with two Pokemon

---
+++ Assets/Scripts/Pokemon/Pokemon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon
+{
+    PokemonBase _base;
+    int level;
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        _base = pBase;
+        level = pLevel;
+    }
+
+    public int Attack
+    {
+        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; } //Same formula as pokemon
+    }
+    public int Defense
+    {
+        get { return Mathf.FloorToInt((_base.Defense * level) / 100f) + 5; } //Same formula as pokemon
+    }
+    public int SpAttack
+    {
+        get { return Mathf.FloorToInt((_base.SpAttack * level) / 100f) + 5; } //Same formula as pokemon
+    }
+    public int SpDefense
+    {
+        get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) + 5; } //Same formula as pokemon
+    }
+    public int Speed
+    {
+        get { return Mathf.FloorToInt((_base.Speed * level) / 100f) + 5; } //Same formula as pokemon
+    }
+    public int MaxHp
+    {
+        get { return Mathf.FloorToInt((_base.MaxHp * level) / 100f) + 10; } //Same formula as pokemon
+    }
+}
