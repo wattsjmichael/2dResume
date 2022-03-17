3911aadb875da52e0cd37ef5add32d5ff884fb81
Michael Watts <wattsjmichael@gmail.com>
2022-03-16 17:57:17 -0700

    Adds BattleHUD, next is COMBAT!!!

---
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState
+{
+    Start,
+    PlayerAction,
+    PlayerMove,
+    EnemyMove,
+    Busy
+}
+
+public class BattleSystem : MonoBehaviour
+{
+    [SerializeField]
+    BattleUnit playerUnit;
+
+    [SerializeField]
+    BattleUnit enemyUnit;
+
+    [SerializeField]
+    BattleHud playerHud;
+
+    [SerializeField]
+    BattleHud enemyHud;
+
+    [SerializeField]
+    BattleDialogBox dialogBox;
+    int currentAction;
+    int currentMove;
+
+    BattleState state;
+
+    private void Start()
+    {
+        StartCoroutine(SetupBattle());
+    }
+
+    public IEnumerator SetupBattle()
+    {
+        playerUnit.Setup();
+        enemyUnit.Setup();
+        playerHud.SetData(playerUnit.Pokemon);
+        enemyHud.SetData(enemyUnit.Pokemon);
+
+        dialogBox.SetMoveNames(playerUnit.Pokemon.Moves);
+        
+        yield return (
+            dialogBox.TypeDialog($"A Wild {enemyUnit.Pokemon.Base.PokeName} appeared!!!")
+        );
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+    
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+
+    }
+
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentAction < 1)
+                ++currentAction;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (currentAction > 0)
+                --currentAction;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+           if(currentAction == 0)
+           {
+               PlayerMove();
+           }
+           else if(currentAction == 1)
+           {
+               Debug.Log("RUN!!!!");
+           }  
+        }
+    }
+    void HandleMoveSelection()
+    {
+            if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count -1)
+                ++currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (currentMove > 0)
+                --currentMove;
+        }
+              if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count -2)
+                currentMove += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (currentMove > 1)
+                currentMove -= 2;
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Pokemon.Moves[currentMove]);
+
+    }
+}
--- Assets/Scripts/Pokemon/Pokemon.cs
+++ Assets/Scripts/Pokemon/Pokemon.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Pokemon
 {
-    PokemonBase _base;
-    int level;
+    public PokemonBase Base {get; set;}
+    public int Level {get; set;}
 
     public int HP {get; set;}
 
@@ -13,16 +13,16 @@ public class Pokemon
 
     public Pokemon(PokemonBase pBase, int pLevel)
     {
-        _base = pBase;
-        level = pLevel;
-        HP = _base.MaxHp;
+        Base = pBase;
+        Level = pLevel;
+        HP = MaxHp;
 
 
         //generate Moves
         Moves = new List<Move>();
-        foreach (var move in _base.LearnableMoves)
+        foreach (var move in Base.LearnableMoves)
         {
-            if (move.Level <= level)
+            if (move.Level <= Level)
             Moves.Add(new Move(move.Base));
 
             if (Moves.Count >= 4)
@@ -32,26 +32,26 @@ public class Pokemon
 
     public int Attack
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; } //Same formula as pokemon
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; } //Same formula as pokemon
     }
     public int Defense
     {
-        get { return Mathf.FloorToInt((_base.Defense * level) / 100f) + 5; } //Same formula as pokemon
+        get { return Mathf.FloorToInt((Base.Defense * Level) / 100f) + 5; } //Same formula as pokemon
     }
     public int SpAttack
     {
-        get { return Mathf.FloorToInt((_base.SpAttack * level) / 100f) + 5; } //Same formula as pokemon
+        get { return Mathf.FloorToInt((Base.SpAttack * Level) / 100f) + 5; } //Same formula as pokemon
     }
     public int SpDefense
     {
-        get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) + 5; } //Same formula as pokemon
+        get { return Mathf.FloorToInt((Base.SpDefense * Level) / 100f) + 5; } //Same formula as pokemon
     }
     public int Speed
     {
-        get { return Mathf.FloorToInt((_base.Speed * level) / 100f) + 5; } //Same formula as pokemon
+        get { return Mathf.FloorToInt((Base.Speed * Level) / 100f) + 5; } //Same formula as pokemon
     }
     public int MaxHp
     {
-        get { return Mathf.FloorToInt((_base.MaxHp * level) / 100f) + 10; } //Same formula as pokemon
+        get { return Mathf.FloorToInt((Base.MaxHp * Level) / 100f) + 10; } //Same formula as pokemon
     }
 }
